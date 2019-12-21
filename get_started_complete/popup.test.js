ef815c5fcddc2c5f115a8b72cfa0485de11930bc
cs8433 <christian.hoerauf@consorsbank.de>
2019-12-21 22:01:28 +0100

    loading color from storage tested

--- get_started_complete/popup.test.js
+++ get_started_complete/popup.test.js
@@ -8,6 +8,12 @@ describe('popup', () => {
         // You need to make the mocked chrome api available to 
         // the popup script
         window.chrome = chrome
+        // Stub the behaviour of chrome.storage.sync.get
+        chrome.storage.sync.get.yields({'color': '#ff0000'})
+        
         require('./popup')
+        const changeColor = document.getElementById('changeColor')
+        expect(changeColor.style.backgroundColor).toEqual('rgb(255, 0, 0)')
+        expect(changeColor.value).toEqual('#ff0000')
     })
 })
