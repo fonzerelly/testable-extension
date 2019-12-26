f388e7271f2554de91a6f17e3dfa2a0e44e22c49
cs8433 <christian.hoerauf@consorsbank.de>
2019-12-26 11:24:55 +0100

    Failed on testing chrome.tabs.executeScript

--- get_started_complete/popup.test.js
+++ get_started_complete/popup.test.js
@@ -39,4 +39,35 @@ describe('popup', () => {
         changeColor.click();
         sinon.assert.calledWith(chrome.tabs.executeScript, 42, {code: 'document.body.style.backgroundColor = "#ff0000";'})
     })
+
+    /**
+     * The following test does not work, because jest does not evaluate
+     * the script manually written into script-tags. If we instead would
+     * find a way to store the code from chrome.tabs.executeScript in a
+     * function, that could be evaluated, that would solve the issue.
+     * Maybe we can return to this test after testing everything else
+     * and refactor the original example code from google accordingly.
+     */
+    xit('should set background color of tab', () => {
+        document.body.innerHTML = `
+            <button id="changeColor"></button>
+        `
+        window.chrome = chrome
+        chrome.storage.sync.get.yields({'color': '#ff0000'})
+        const fakeTab =  {
+            id: 42
+        }
+        chrome.tabs.query.yields([fakeTab]) // be sure to pass an array as the api expects it
+        
+        require('./popup')
+        const changeColor = document.getElementById('changeColor')
+        changeColor.click();
+        const { code } = chrome.tabs.executeScript.firstCall.args[1]
+        document.body.innerHTML = `
+            <script>
+            ${code}
+            </script>
+        `
+        expect(document.body.style.backgroundColor).toEqual('rgb(255,0,0)')
+    })
 })
