6b9e1ff5a9665f11360a7fa07262ac632834f571
fonzerelly <christian.hoerauf@gmail.com>
2019-12-28 04:13:19 +0100

    options: failed to test console.log

--- get_started_complete/options.test.js
+++ get_started_complete/options.test.js
@@ -9,16 +9,22 @@ describe('options', () => {
     })
 
     describe('buttons', () => {
-        let buttons
+        let buttons, consoleLogSpy
         beforeEach(() => {
             document.body.innerHTML = `
                 <div id="buttonDiv"></div>
             `
             window.chrome = chrome
+            consoleLogSpy = jest.spyOn(console, 'log')
             require('./options')
             const buttonDiv = document.getElementById('buttonDiv')
             buttons = Array.from(buttonDiv.querySelectorAll('button'))
         })
+
+        afterEach(() => {
+            consoleLogSpy.mockRestore();
+        })
+
         it('should create 4 buttons with four colors', () => {
             expect(buttons.length).toEqual(4)
         })
@@ -69,6 +75,33 @@ describe('options', () => {
                     { color: '#'+rgbHex(color) },
                     sinon.match.func
                 )
+            })
+        })
+
+        /**
+         * It would have been a nice test, to check if the callback of chrome.storage.syn.set does
+         * what was expected in its call back (to call console.log)
+         * But as it turns out, the callback does not seem to be called in the same event loop iteration
+         * as the click for the button. Additionally, since it is a simple callback and no promise or
+         * similar accessable "event in the future", you can not tell when it will be processed and
+         * take measures to implement it accordingly. Therefore it seems to me to be impossible to test
+         * without changing the original code. Also the call to console.log might be something that
+         * can be omitted for tests, since there is nothing else this callback does.
+         * If the callback would do more, I would highly recommend it to refactor it to be accessable as
+         * regular function or method and test that instead, without the callback burdon of chrome.
+         * 
+         * PS: There is a possibility to test the callback of chrome.storage.sync.set: By calling sinons yield function on it.
+         * chrome.storage.syn.set.yield()
+         * But when you have a look at the current implementiation, you will see, that the color value used byconsole.log
+         * is not passed in the callback (or at least it is not used from there). Instead the "item" from  the outer 
+         * button creation loop, from the closure, is used. So we can not pass in the color value by yielding
+         */
+        xit('should trigger chrome.storage.sync.set with color', () => {
+            buttons.forEach((button, index) => {
+                const color = button.style.backgroundColor
+                button.click()
+          
+                expect(consoleLogSpy.mock.calls).toBe(`color is ${color}`)
             })
         })
     })
